84829a94188d36fea28497ebb9d097ad698a4a8b
Big Kahuna <35747818+A3R0DYN4M1C5@users.noreply.github.com>
2020-03-07 18:49:27 -0500

    Started working with NewsApi
    Created a python file to communicate with the News Api. It grabs top headlines and dumps the titles, urls, and images into a csv for use later

---
+++ News.py
@@ -0,0 +1,25 @@
+from newsapi import NewsApiClient
+import datetime as dt
+import csv
+
+# init
+newsapi = NewsApiClient(api_key='312e4cb0c72d460982f988d5cb036d57')
+
+data = newsapi.get_top_headlines(q='', language='en', page_size=20)
+
+print(data['articles'])
+
+
+with open('News.csv','w',newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['TITLE','URL','IMAGE'])
+    for i in data['articles']:
+        writer.writerow([i['title'],i['url'],i['urlToImage']])
+    
+
+
+
+#for x,y in enumerate(articles):
+#    print(f'{x}  {y["title"]}')
+
+    
