1ac751d88a62cec89b7c28f6a3aed0e06239af3d
Big Kahuna <35747818+A3R0DYN4M1C5@users.noreply.github.com>
2020-04-12 15:58:32 -0400

    New update

--- News.js
+++ News.js
@@ -1,10 +1,11 @@
+
 var NewsData = new XMLHttpRequest();
 NewsData.onreadystatechange = function (){
     if (this.readyState == 4 && this.status == 200){
         console.log(this.responseText);
         var lines = this.responseText.split("\n");
         var strOut = "<ul>";
-        for(i=0; i<lines.length; i++){
+        for(i=1; i<lines.length-1; i++){
             var field = lines[i].split(",");
             strOut += "<li><a href='" + field[1];
             strOut += "' target='_blank'>" + field[0];
@@ -16,3 +17,4 @@ NewsData.onreadystatechange = function (){
 };
     NewsData.open("GET","News.csv", true);
     NewsData.send();
+ 
